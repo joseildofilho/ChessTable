f7da4f15ee3b224de019de5c50f30bba804e65f0
Joseildo Filho <filhojoseildo@gmail.com>
2021-06-19 20:11:58 -0300

    git ignore and moving home_page to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:chess/chess.dart' hide State;
+import 'package:chesstable/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart';
 
 void main() {
   runApp(ChessTable());
@@ -14,32 +13,9 @@ class ChessTable extends StatefulWidget {
 class _ChessTableState extends State<ChessTable> {
   @override
   Widget build(BuildContext context) => MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: HomePage(),
-    );
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final chess = Chess();
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Chessboard(
-              boardSize: 600,
-              fen: chess.generate_fen(),
-              onMove: (move) {
-                chess.move({
-                  'from': move.from,
-                  'to': move.to,
-                });
-                setState(() {});
-              }),
-        ),
+        title: 'ChessTable',
+        theme: ThemeData.dark(),
+        home: HomePage(),
       );
 }
+
